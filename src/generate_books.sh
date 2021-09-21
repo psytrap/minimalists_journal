9b41c081a63e8706fa53ae2b81f7f0aa1d13ec2f
psytrap <michael.brenner.software@gmail.com>
2021-09-21 23:16:01 +0200

    adding debug output, books without bleed

--- src/generate_books.sh
+++ src/generate_books.sh
@@ -11,6 +11,8 @@ book72pages()
   local title=$2
   local pages
   local pages_with_dir
+  
+  echo ; echo "Defining title and planner section..."
   pages="title_page.pdf"
   pages="$pages blank.pdf"
   pages="$pages blank.pdf"
@@ -20,21 +22,27 @@ book72pages()
   pages="$pages $planner"
   pages="$pages $planner"
 
-  for i in $(seq 57) ; do pages="$pages bullet_sheet_g5mm_i80.pdf" ; done
+  echo ; echo "Defining bullet section..."
+  for i in $(seq 57) ; do pages="$pages bullet_sheet_with_date_g5mm_i80.pdf" ; done
 
+  echo ; echo "Defining back cover page..."
   pages="$pages blank.pdf"
   pages="$pages blank.pdf"
-  
   
   for i in $pages ; do pages_with_dir="$pages_with_dir ${PDF_DIR}/$i" ; done
   
+  echo ; echo "Merging pdfs and adding bleed area..."
   echo "$pages_with_dir"
-  gs -o "$BOOK_DIR/${title}_72pages.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=314 -dDEVICEHEIGHTPOINTS=437 \
+  gs -o "$BOOK_DIR/${title}_72pages.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=297 -dDEVICEHEIGHTPOINTS=420 \
+    -dFIXEDMEDIA -c "<</PageOffset [8 9]>> setpagedevice" -f $pages_with_dir
+  gs -o "$BOOK_DIR/${title}_72pages_with_bleed.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=314 -dDEVICEHEIGHTPOINTS=437 \
     -dFIXEDMEDIA -c "<</PageOffset [8 9]>> setpagedevice" -f $pages_with_dir
   #gs -o "$BOOK_DIR/work_72pages.pdf" -sDEVICE=pdfwrite $pages_with_dir
 }
 
 
+echo ; echo "Creating journal for work..."
 book72pages weekly_planner_work.pdf monthly_work
 
+echo ; echo "Creating journal for personal use..."
 book72pages weekly_planner.pdf monthly
--- src/generate_pdf.sh
+++ src/generate_pdf.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-
+echo ; echo "Clean-up..."
 rm -v ../pdf/A6/*
 rm -v ../pdf/4x6inch/*
 rm -v ../pdf/A5/*
 
+echo ; echo "Convert docs..."
 loffice --convert-to pdf --outdir ../pdf/A6/ ./blank.odt ./title_page.odt
 loffice --convert-to pdf --outdir ../pdf/A6/ ./weekly*.ods
 
 
-rm -v bullet_sheet*svg
+echo ; echo "Generate bullet sheets..."
+rm -vf bullet_sheet*svg
 grid="5"
-echo "Grid: $grid"
+echo ; echo "- Grid: ${grid}mm"
 for i in 70 80 90 ; do
-  echo "Intensity: $i"
+  echo ; echo "- Intensity: $i"
   # A6
   python3 ./generate_bullet_sheet.py --grid $grid --intensity $i --output bullet_sheet_g${grid}mm_i$i.svg
   python3 ./generate_bullet_sheet.py --grid $grid --intensity $i --date --output bullet_sheet_with_date_g${grid}mm_i$i.svg
@@ -26,10 +28,10 @@ for i in 70 80 90 ; do
   inkscape ./bullet_sheet_with_date_A5_g${grid}mm_i$i.svg --export-pdf="../pdf/A5/bullet_sheet_with_date_g${grid}mm_i$i.pdf"
 done
 
-sleep 10
+echo ; sleep 5
 rm -v *svg
 
-# Convert to 4x6 inch
+echo ; echo "Convert to 4x6 inch..."
 for pdf in ../pdf/A6/* ; do
   pdf=$(basename $pdf)
   echo $pdf
@@ -39,6 +41,7 @@ done
 
 
 # weekly / tracker / quick
+echo ; echo "Convert A5 docs..."
 loffice --convert-to pdf --outdir ../pdf/A5/ ./A5*.ods
 loffice --convert-to pdf --outdir ../pdf/A5/ ./calendars/A5*.ods
 
