cd593895d1a9bc5991c909f3fa67a95d8c5f4c28
psytrap <44663346+psytrap@users.noreply.github.com>
2022-04-07 20:05:36 +0200

    Kalender + vergessen...

--- src/generate_books.sh
+++ src/generate_books.sh
@@ -22,8 +22,9 @@ book72pages()
   pages="$pages $planner"
   pages="$pages $planner"
 
+  # pages = x + 15   # x + 57 = 72  # x = 15
   echo ; echo "Defining bullet section..."
-  for i in $(seq 57) ; do pages="$pages bullet_sheet_with_date_g5mm_i80.pdf" ; done
+  for i in $(seq 45) ; do pages="$pages bullet_sheet_with_date_g5mm_i80.pdf" ; done
 
   echo ; echo "Defining back cover page..."
   pages="$pages blank.pdf"
@@ -33,11 +34,11 @@ book72pages()
   
   echo ; echo "Merging pdfs and adding bleed area..."
   echo "$pages_with_dir"
-  gs -o "$BOOK_DIR/${title}_72pages.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=297 -dDEVICEHEIGHTPOINTS=420 \
+  gs -o "$BOOK_DIR/${title}_60pages.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=297 -dDEVICEHEIGHTPOINTS=420 \
+    -dFIXEDMEDIA -c "<</PageOffset [0 0]>> setpagedevice" -f $pages_with_dir
+  gs -o "$BOOK_DIR/${title}_60pages_with_bleed.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=314 -dDEVICEHEIGHTPOINTS=437 \
     -dFIXEDMEDIA -c "<</PageOffset [8 9]>> setpagedevice" -f $pages_with_dir
-  gs -o "$BOOK_DIR/${title}_72pages_with_bleed.pdf" -sDEVICE=pdfwrite -dDEVICEWIDTHPOINTS=314 -dDEVICEHEIGHTPOINTS=437 \
-    -dFIXEDMEDIA -c "<</PageOffset [8 9]>> setpagedevice" -f $pages_with_dir
-  #gs -o "$BOOK_DIR/work_72pages.pdf" -sDEVICE=pdfwrite $pages_with_dir
+  #gs -o "$BOOK_DIR/work_60pages.pdf" -sDEVICE=pdfwrite $pages_with_dir
 }
 
 
